7e915e557e43136dd03b7159735be9877fbdb011
Landon Wood <landonw@smu.edu>
2021-09-22 00:29:56 -0500

    Add calculateTargetAttributes, comments, reformat variables

--- core.lua
+++ core.lua
@@ -36,6 +36,7 @@ function EmberCourtHelper:ChatCommand(input)
     end 
 end
 
+-- Determines which Ember Court friend factions are not at maximum rank yet
 function EmberCourtHelper:GetIncompleteReps()
     local reps = {}
     for i = 1838, 1853 do -- Faction IDs 1838 through 1853 are Ember Court friend factions
@@ -48,14 +49,139 @@ function EmberCourtHelper:GetIncompleteReps()
     return reps
 end
 
+-- Options for target guest dropdown menus
+local slot1_options = {"Sika", "Plague Deviser Marileth", "Choofa", "Cryptkeeper Kassir"}
+local slot2_options = {"Kleia and Pelagos", "Grandmaster Vole", "Droman Aliothe", "Stonehead"}
+local slot3_options = {"Polemarch Adrestes", "Alexandros Mograine", "Hunt-Captain Korayn", "Rendle and Cudgelface"}
+local slot4_options = {"Mikanikos", "Baroness Vashj", "Lady Moonberry", "The Countess"}
+
+-- Tables for guest preferences
+local slot1_prefs = {
+    {"Clean"}, -- Sika
+    {"Messy"}, -- Marileth
+    {"Exciting"}, -- Choofa
+    {"Formal"} -- Kassir
+}
+local slot2_prefs = {
+    {"Humble"}, -- Kleia and Pelagos
+    {"Dangerous"}, -- Grandmaster Vole
+    {"Relaxing"}, -- Droman Aliothe
+    {"Casual"} -- Stonehead
+}
+local slot3_prefs = {
+    {"Clean", "Formal"}, -- Polemarch Adrestes
+    {"Safe", "Humble"}, -- Alexandros Mograine
+    {"Dangerous", "Casual"}, -- Hunt-Captain Korayn 
+    {"Messy", "Relaxing"} -- Rendle and Cudgelface
+}
+local slot4_prefs = {
+    {"Clean", "Safe", "Humble"}, -- Mikanikos
+    {"Dangerous", "Decadent", "Exciting"}, -- Baroness Vashj
+    {"Messy", "Exciting", "Casual"}, -- Lady Moonberry
+    {"Decadent", "Relaxing", "Formal"} -- The Countess
+}
+
+-- Sum up attributes that each selected guest prefers and return strings for moods to target.
+function EmberCourtHelper:calculateTargetAttributes(slot1_choice, slot2_choice, slot3_choice, slot4_choice)
+
+    local messyCtr = 0
+    local cleanCtr = 0
+    local safeCtr = 0 
+    local dangerousCtr = 0  
+    local humbleCtr = 0
+    local decadentCtr = 0
+    local excitingCtr = 0
+    local relaxingCtr = 0
+    local formalCtr = 0
+    local casualCtr = 0
+
+    if slot1_choice == 1 then cleanCtr = cleanCtr + 1
+    elseif slot1_choice == 2 then messyCtr = messyCtr + 1
+    elseif slot1_choice == 3 then excitingCtr = excitingCtr + 1
+    elseif slot1_choice == 4 then formalCtr = formalCtr + 1
+    end
+
+    if slot2_choice == 1 then humbleCtr= humbleCtr + 1
+    elseif slot2_choice == 2 then dangerousCtr = dangerousCtr + 1
+    elseif slot2_choice == 3 then relaxingCtr = relaxingCtr + 1
+    elseif slot2_choice == 4 then casualCtr = casualCtr + 1
+    end
+
+    if slot3_choice == 1 then 
+        cleanCtr = cleanCtr + 1
+        formalCtr = formalCtr + 1
+    elseif slot3_choice == 2 then 
+        safeCtr = safeCtr + 1
+        humbleCtr = humbleCtr + 1
+    elseif slot3_choice == 3 then 
+        dangerousCtr = dangerousCtr + 1
+        casualCtr = casualCtr + 1
+    elseif slot3_choice == 4 then 
+        messyCtr = messyCtr + 1
+        relaxingCtr = relaxingCtr + 1
+    end
+
+    if slot4_choice == 1 then 
+        cleanCtr = cleanCtr + 1
+        safeCtr = safeCtr + 1
+        humbleCtr = humbleCtr + 1
+    elseif slot4_choice == 2 then 
+        dangerousCtr = dangerousCtr + 1
+        decadentCtr = decadentCtr + 1
+        excitingCtr = excitingCtr + 1
+    elseif slot4_choice == 3 then 
+        messyCtr = messyCtr + 1
+        excitingCtr = excitingCtr + 1
+        casualCtr = casualCtr + 1
+    elseif slot4_choice == 4 then 
+        decadentCtr = decadentCtr + 1
+        relaxingCtr = relaxingCtr + 1
+        formalCtr = formalCtr + 1
+    end
+
+    local cleanliness 
+    local danger
+    local decadence
+    local excitement
+    local formality
+
+    if cleanCtr > messyCtr then cleanliness = "Clean"
+    elseif messyCtr > cleanCtr then cleanliness = "Messy"
+    -- Determine what if values are equal. Choose default? 
+    -- If you have the safeguards for only factoring in non maxed friends then either attribute would end up being beneficial i think.
+    -- if nil is returned for an attribute, then do a random choice of a table?
+    end
+
+    if safeCtr > dangerousCtr then danger = "Safe"
+    elseif dangerousCtr > safeCtr then danger = "Dangerous"
+    end
+
+    if humbleCtr > decadentCtr then decadence = "Humble"
+    elseif decadentCtr > humbleCtr then decadence = "Decadent"
+    end
+    
+    if excitingCtr > relaxingCtr then excitement = "Exciting"
+    elseif relaxingCtr > excitingCtr then excitement = "Relaxing"
+    end
+
+    if formalCtr > casualCtr then formality = "Formal"
+    elseif casualCtr > formalCtr then formality = "Casual"
+    end
+
+    return cleanliness, danger, decadence, excitement, formality
+end
+
+
 function EmberCourtHelper:CreateWindow()
 
+    -- Initialize main frame
     local frame = AceGUI:Create("Frame")
     frame:SetTitle("EmberCourtHelper")
     frame:SetStatusText("Version " .. version)
     frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
     frame:SetLayout("List")
 
+    -- TabGroup to contain TabGroups for displaying faction standings
     local repGroups = AceGUI:Create("TabGroup")
     repGroups:SetTitle("Incomplete Guest Reputations")
     frame:AddChild(repGroups)
@@ -88,7 +214,7 @@ function EmberCourtHelper:CreateWindow()
     repGroups:AddChild(goodfriends)
     goodfriends:SetRelativeWidth(.21)
 
-
+    -- Determine which friend factions are incomplete and add to display
     local incompleteReps = EmberCourtHelper:GetIncompleteReps()
     for name, standing in pairs(incompleteReps) do
         local repLine = AceGUI:Create("Label")
@@ -106,12 +232,6 @@ function EmberCourtHelper:CreateWindow()
             goodfriends:AddChild(repLine)
         end 
     end
-
-
-    local slot1_options = {"Sika", "Plague Deviser Marileth", "Choofa", "Cryptkeeper Kassir"}
-    local slot2_options = {"Kleia and Pelagos", "Grandmaster Vole", "Droman Aliothe", "Stonehead"}
-    local slot3_options = {"Polemarch Adrestes", "Alexandros Mograine", "Hunt-Captain Korayn", "Rendle and Cudgelface"}
-    local slot4_options = {"Mikanikos", "Baroness Vashj", "Lady Moonberry", "The Countess"}
 
 
     -- Create dropdown frame and add child slot frames to select guests
@@ -136,7 +256,6 @@ function EmberCourtHelper:CreateWindow()
     slot2:SetRelativeWidth(.25)
     slot2:SetLabel("Select 2nd Guest")
     slot2:SetList(slot2_options)
-    slot2:SetValue("Kleia and Pelagos")
     slot2:SetCallback("OnValueChanged", function(widget, event, choice) slot2_choice = choice end)
     dropdownFrame:AddChild(slot2)
 
@@ -145,7 +264,6 @@ function EmberCourtHelper:CreateWindow()
     slot3:SetRelativeWidth(.25)
     slot3:SetLabel("Select 3rd Guest")
     slot3:SetList(slot3_options)
-    slot3:SetValue("Polemarch Adrestes")
     slot3:SetCallback("OnValueChanged", function(widget, event, choice) slot3_choice = choice end)
     dropdownFrame:AddChild(slot3)
 
@@ -154,88 +272,23 @@ function EmberCourtHelper:CreateWindow()
     slot4:SetRelativeWidth(.25)
     slot4:SetLabel("Select 4th Guest")
     slot4:SetList(slot4_options)
-    slot4:SetValue("Mikanikos")
     slot4:SetCallback("OnValueChanged", function(widget, event, choice) slot4_choice = choice end)
     dropdownFrame:AddChild(slot4)
     
-    -- Tables for guest preferences
-    local slot1_prefs = {
-        {"Clean"}, -- Sika
-        {"Messy"}, -- Marileth
-        {"Exciting"}, -- Choofa
-        {"Formal"} -- Kassir
-    }
-    local slot2_prefs = {
-        {"Humble"}, -- Kleia and Pelagos
-        {"Dangerous"}, -- Grandmaster Vole
-        {"Relaxing"}, -- Droman Aliothe
-        {"Casual"} -- Stonehead
-    }
-    local slot3_prefs = {
-        {"Clean", "Formal"}, -- Polemarch Adrestes
-        {"Safe", "Humble"}, -- Alexandros Mograine
-        {"Dangerous", "Casual"}, -- Hunt-Captain Korayn 
-        {"Messy", "Relaxing"} -- Rendle and Cudgelface
-    }
-    local slot4_prefs = {
-        {"Clean", "Safe", "Humble"}, -- Mikanikos
-        {"Dangerous", "Decadent", "Exciting"}, -- Baroness Vashj
-        {"Messy", "Exciting", "Casual"}, -- Lady Moonberry
-        {"Decadent", "Relaxing", "Formal"} -- The Countess
-    }
-
-    
-    -- move slot_prefs and slot_options to global positions above OnInitialize
-    -- ensure that these work with the code still set up as it is
-    -- then declare variables
-
-    local messyCtr = 0
-    local cleanCtr = 0
-    local safeCtr = 0 
-    local dangerousCtr = 0  
-    local humbleCtr = 0
-    local decadentCtr = 0
-    local excitingCtr = 0
-    local relaxingCtr = 0
-    local formalCtr = 0
-    local casualCtr = 0
-
-    local cleanliness
-    
-
-    for i = 1,4 do
-        -- if guest is not completed
-        if slot1_prefs[i] == "Messy" then messyCtr = MessyCtr + 1
-        elseif slot1_prefs[i] == "Clean" then cleanCtr = cleanCtr + 1
-        elseif slot1_prefs[i] == "Safe" then safeCtr = safeCtr + 1
-        elseif slot1_prefs[i] == "Dangerous" then Ctr = cleanCtr + 1
-        elseif slot1_prefs[i] == "Humble" then cleanCtr = cleanCtr + 1
-    end
-
-    end
-
-
-    --local attribute_sums = { messyCtr, }
-
-
-    -- function to sum each attribute and choose winnner
-    -- pass it an attribute name - messy or clean, humble or decadent
-    -- sumAttributeWeight("formal")
-    -- returns integer for how many times a thing appears
-    -- then compare the sum of the two competing attributes
-    -- choose the bigger one to display as target attribute.
-
-    -- sum up each time an attribute appears in a guest's preferences matrix.
-    -- only if that guest is not at full rank. 
-    -- or greater weight to unmaxed guests? 1 for best friend, 2 for non best friend.
-    -- whichever attribute out of the two is highest, display that one in the targets. 
 
     local calc_button = AceGUI:Create("Button")
     calc_button:SetText("Calculate Target Attributes")
     calc_button:SetWidth(300)
-    calc_button:SetCallback("OnClick", function() print(slot4_prefs[3][1], slot4_prefs[3][2], slot4_prefs[3][3]) end)
+    calc_button:SetCallback("OnClick", 
+        function() 
+            local cleanliness, danger, decadence, excitement, formality = EmberCourtHelper:calculateTargetAttributes(slot1_choice, slot2_choice, slot3_choice, slot4_choice) 
+            print (cleanliness, danger, decadence, excitement, formality)    
+        end 
+    )
     frame:AddChild(calc_button)
     
+    -- assign strings to displays
+    -- add check for whether or not faction is best friend. How to implement?
 
 end
 
